59e024342ccff2503cbe86c5a8b8b5b5e08dc4f1
sharj0 <sharjeela1999@gmail.com>
2024-12-13 16:13:22 -0500

    updates

--- convert_xml_paths_to_branch_Sharj.py
+++ convert_xml_paths_to_branch_Sharj.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from lxml import etree
 
 """CHANGE THIS STRING FOR BRANCH NAME (DO NOT PUT ANY SLASHES)"""
-branch_name_string = "sharj_ceate_modular_match_versions_zip"
+branch_name_string = "sharj_ceate_modular_match_versions_zip_py"
 
 
 
@@ -34,15 +34,16 @@ def convert_xml_paths_to_branch(xml_file_path=(Path(__file__).parent / "plugins_
         old_plugin_zip_path = plugin.find("download_url").text
         old_plugin_icon_path = plugin.find("icon").text
 
-        latter_plugin_path = Path(*Path(old_plugin_zip_path).parts[-2:]).as_posix()
-        latter_icon_path = Path(*Path(old_plugin_icon_path).parts[-3:]).as_posix()
+        if not old_plugin_zip_path.startswith(branch_URI_directory):
+            latter_plugin_path = Path(*Path(old_plugin_zip_path).parts[-2:]).as_posix()
+            latter_icon_path = Path(*Path(old_plugin_icon_path).parts[-3:]).as_posix()
 
-        new_plugin_zip_path = target_branch_dir + "/" + latter_plugin_path
-        new_plugin_icon_path = target_branch_dir + "/" + latter_icon_path
+            new_plugin_zip_path = target_branch_dir + "/" + latter_plugin_path
+            new_plugin_icon_path = target_branch_dir + "/" + latter_icon_path
 
-        plugin.find("download_url").text = new_plugin_zip_path
-        plugin.find("icon").text = new_plugin_icon_path
-        print(f"XML Name: {new_name}  | ZIP PATH: {new_plugin_zip_path}")
+            plugin.find("download_url").text = new_plugin_zip_path
+            plugin.find("icon").text = new_plugin_icon_path
+            print(f"XML Name: {new_name}  | ZIP PATH: {new_plugin_zip_path}")
 
     tree.write(xml_file_path, pretty_print=True, xml_declaration=False, encoding="UTF-8")
 
--- revert_xml_to_main_Sharj.py
+++ revert_xml_to_main_Sharj.py
@@ -27,15 +27,17 @@ def revert_xml_paths_to_main(xml_file_path=(Path(__file__).parent / "plugins_lea
         old_plugin_zip_path = plugin.find("download_url").text
         old_plugin_icon_path = plugin.find("icon").text
 
-        latter_plugin_path = Path(*Path(old_plugin_zip_path).parts[-2:]).as_posix()
-        latter_icon_path = Path(*Path(old_plugin_icon_path).parts[-3:]).as_posix()
+        if not old_plugin_zip_path.startswith(MAIN_REPO_URI):
+            latter_plugin_path = Path(*Path(old_plugin_zip_path).parts[-2:]).as_posix()
+            latter_icon_path = Path(*Path(old_plugin_icon_path).parts[-3:]).as_posix()
 
-        new_plugin_zip_path = repo_url + "/" + latter_plugin_path
-        new_plugin_icon_path = repo_url + "/" + latter_icon_path
+            new_plugin_zip_path = repo_url + "/" + latter_plugin_path
+            new_plugin_icon_path = repo_url + "/" + latter_icon_path
 
-        plugin.find("download_url").text = new_plugin_zip_path
-        plugin.find("icon").text = new_plugin_icon_path
-        print(f"XML Name: {new_name}  | ZIP PATH: {new_plugin_zip_path}")
+            plugin.find("download_url").text = new_plugin_zip_path
+            plugin.find("icon").text = new_plugin_icon_path
+
+            print(f"XML Name: {new_name}  | ZIP PATH: {new_plugin_zip_path}")
 
 
     tree.write(xml_file_path, pretty_print=True, xml_declaration=False, encoding="UTF-8")
