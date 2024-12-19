00da798e1fcc3363b5f05f8dd34acf191bdc19c2
sharj0 <sharjeela1999@gmail.com>
2024-12-19 09:22:32 -0500

    testing

--- ROSORPlugins/match_versions_and_zip_Sharj.py
+++ ROSORPlugins/match_versions_and_zip_Sharj.py
@@ -92,21 +92,27 @@ def autozip_files_main(folders_that_need_updating,
                        plugin_dir=os.path.dirname(__file__)):
     #I'm reusing Peter's code
 
-    # Check if the provided plugin directory exists
+
     if not os.path.isdir(plugin_dir):
+
         print(f"The directory {plugin_dir} does not exist.")
+
         return
 
-    #Iterate through all the folders in the current directory
+
     for root, dirs, files in os.walk(plugin_dir):
+
         for dir_name in dirs:
+
             #Check if directory has the prefix which is our indicator/standard for plugins
             if dir_name.startswith(plugin_prefix):
+
                 if dir_name in folders_that_need_updating:
+
                     print(f'zipping folder {dir_name} ... ')
-                    #Calls separate function in file that checks whether there is a difference between the zipped plugin and the unzipped one in current directory
+
                     if is_archive_folder_different(dir_name):
-                        #uses the zipfile library to write the plugin folder into an archive with the plugin folder name
+
                         zip_file(dir_name)
 
 #Compares a zipped folder to an unzipped folder and should return true if any file is different and false when it's compared all the files and fails to find a difference (the default path is current directory)
@@ -148,20 +154,18 @@ def is_archive_folder_different(folder, directory=os.path.dirname(__file__)):
 #Created a dedicated function to zip files as it might be used later
 #Defaults to the folder being in the working directory
 def zip_file(folder, directory=os.path.dirname(__file__)):
-    #Sets the path to the folder
+
     folder_path = os.path.join(directory, folder)
 
     #Creates a ZipFile instance that creates a .zip using the folder and path
     with zipfile.ZipFile(folder_path + ".zip", mode="w", compression=zipfile.ZIP_LZMA) as archive:
 
-        #Goes through every file in the chosen folder
         for root, dirs, files in os.walk(folder_path):
             for file in files:
                 #writes the file and any parent folder if applicable to the new archive (I got this from stack overflow and it just worked)
                 archive.write(os.path.join(root, file),
                               os.path.relpath(os.path.join(root, file), os.path.join(folder_path, "..")))
 
-    #Prints out that its done its job
     print("zipped: " + folder)
 
 
@@ -172,16 +176,16 @@ def match_xml_version_main(folders_that_need_updating,
                            current_path=os.path.dirname(__file__),
                            update_date=False,
                            increment_all=False):
-    #gets parent directory for xml file path
+
     parent_dir = os.path.dirname(current_path)
     xml_file_path = os.path.join(parent_dir, xml_file_name)
 
-    #ends function early if it cannot find the xml file in the parent folder
+
     if not os.path.isfile(xml_file_path):
         print("given plugin xml doesn't exist in the parent directory")
         return None
 
-    #uses elementree to read xml data
+
     tree = ET.parse(xml_file_path)
     root = tree.getroot()
 
@@ -201,25 +205,25 @@ def match_xml_version_main(folders_that_need_updating,
             continue
         plugin_folder_path = os.path.join(current_path, plugin_folder)
 
-        # creates a metadata.txt path by appending it to the plugin folder path above as every plugin should have a metadata text file
+
         metadata_path = os.path.join(plugin_folder_path, "metadata.txt")
 
-        # prints out plugin name and xml version in the console
+
         print(f"xml version for {plugin_folder}: {xml_version}")
 
         #creates a version string with today's date using the make_version_todays_date function
         xml_date_version = make_version_todays_date(xml_version)
 
-        #checks if the written xml_version is different from the todays
+
         if update_date and Version(xml_version) != Version(xml_date_version):
 
-            #since the xml is different, then the new one needs to be written (setting the boolean to true)
+
             change_xml = True
 
-            #replaces the xml version with current date
+
             xml_version = xml_date_version
 
-            #notifies user about the updated date version
+
             print(f"updated xml version to todays date: {xml_version}")
 
         #setting up a conditional boolean on whether to write to the metadata file
@@ -237,55 +241,51 @@ def match_xml_version_main(folders_that_need_updating,
                 #isolates the line with the plugin's version that we want to check
                 metadata_version = metadata[4][8:-1]
 
-                # prints the plugin name and metadata version in the console
                 print(f"metadata version for {plugin_folder}: {metadata_version}")
 
                 # creates a version string with today's date using the make_version_todays_date function
                 metadata_date_version = make_version_todays_date(metadata_version)
 
-                # checks if the written metadata_version is different from the todays
+
                 if update_date and Version(metadata_version) != Version(metadata_date_version):
 
                     # since the metadata is different, then the new one needs to be written (setting the boolean to true)
                     change_metadata = True
 
-                    # replaces the xml version with current date
                     metadata_version = metadata_date_version
 
-                    # notifies user about the updated date version
                     print(f"updated xml version to todays date: {metadata_version}")
 
                 #if deciding to increment each plugin to save time this calls the function to add one to the right most decimal
                 if increment_all:
+
                     metadata_version = increment_two_decimal_version_string(metadata_version, target_index=-1)
+
                     xml_version = increment_two_decimal_version_string(xml_version, target_index=-1)
+
                     change_xml = True
+
                     change_metadata = True
+
                     print(f"xml incremented: {xml_version} | metadata incremented: {metadata_version} | (highest is used to write)")
 
 
                 #This if statement compares the versions using packaging.version library and decides which value to change based on the larger version
                 if Version(metadata_version) < Version(xml_version):
 
-                    # If metadata's version is less than the xml one, then metadata needs to change
                     change_metadata = True
 
-                    #sets metadata version the the greater to match
                     metadata_version = xml_version
 
-                    #statement in console to show user that the metadata is changed to match the xml
                     print(f"modified metadata for {plugin_folder}\n")
 
 
                 elif Version(metadata_version) > Version(xml_version):
 
-                    #If xml's version is less than the metadata one, then xml needs to change
                     change_xml = True
 
-                    #sets xml version to the greater to match
                     xml_version = metadata_version
 
-                    # statement in console to show user that the xml is changed to match the metadata
                     print(f"modified xml for {plugin_folder}\n")
 
                 else:
@@ -295,7 +295,6 @@ def match_xml_version_main(folders_that_need_updating,
 
                     pass
 
-            #Checks boolean to change metadata
             if change_metadata:
 
                 # Alters the 4th line in metadata (which should be version=x.x.x in the current template) to match xml version
@@ -309,10 +308,10 @@ def match_xml_version_main(folders_that_need_updating,
             #If metadata file doesn't exist, then move on (this else can be omitted)
             pass
 
-        #Checks boolean to change xml
+
         if change_xml:
 
-            # alters the text in the xml "version" attribute to match the metadata version
+            #Alters the text in the xml "version" attribute to match the metadata version
             plugin.set('version', xml_version)
 
             #Overwrites xml file with modified versions for all plugins that have changed (I think this can go outside the for loop so it only writes once, but oh well, I can't be bothered to try and debug)
@@ -326,19 +325,14 @@ def increment_two_decimal_version_string(version="1.0.0", target_index=-1):
     if (not Version(version)) or target_index > len(version.split(".")):
         return None
 
-    # splits the version string based on the delimiter "." and converts them into an integer array
     version_ints = [int(split_number) for split_number in version.split(".")]
 
-    # incremets the targeted value in the integer array
     version_ints[target_index] += 1
 
-    # converts the integer array into a string array with the incremented numerical
     new_version_str = [str(new_split_number) for new_split_number in version_ints]
 
-    # joins the string array for the new incremented version
     new_version = '.'.join(new_version_str)
 
-    # return the incremented version
     return new_version
 
 
@@ -378,7 +372,6 @@ def make_version_todays_date(version="1.0.0"):
         #create the larger array into a an array full of strings
         new_version_str = [str(new_split_number) for new_split_number in version_ints]
 
-        #join the string array with "." as a delimiter
         new_version = ".".join(new_version_str)
 
         #recurse the appended string through until it meets the array size of 5 (it will continue appending zeros until it meets the first if statement in this nest)
@@ -387,10 +380,8 @@ def make_version_todays_date(version="1.0.0"):
     # converts the integer array into a string array with the incremented numerical
     new_version_str = [str(new_split_number) for new_split_number in version_ints]
 
-    # joins the string array for the new incremented version
     new_version = ".".join(new_version_str)
 
-    # return the incremented version
     return new_version
 
 # Sample function for version update logic
