a8bf4f32be4ce2e4c5e5d652f46da907742f75c1
pyotyr <89808618+pyotyr@users.noreply.github.com>
2025-02-21 14:38:43 -0500

    updataed cliff fixer

--- ROSORPlugins/De-experimentalize_a_plugin.py
+++ ROSORPlugins/De-experimentalize_a_plugin.py
@@ -2,7 +2,7 @@
 import os
 import shutil
 
-target_plugin = r"C:\Users\pyoty\Documents\GitHub\test_braaahnch\sharj0.github.io\ROSORPlugins\PETER_ROSOR_geotiff_offset_editor"
+target_plugin = r"C:\Users\pyoty\Documents\GitHub\test_braaahnch\sharj0.github.io\ROSORPlugins\PETER_ROSOR_geotiff_offset_editor_exp"
 
 # ------------------------------------------------------------------------------
 # This function validates whether a string ends in "exp" or "experimental"
